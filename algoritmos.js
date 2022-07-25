7a10f3028c231bd29b122038d8315478d8a68728
rodrigo laime <rodrigolaime141@gmail.com>
2022-07-25 17:16:57 -0300

    commit 2

---
+++ algoritmos.js
@@ -0,0 +1,76 @@
+var a;
+var b;
+var c;
+
+function suma(a,b)
+{
+    return c=a +b;
+
+}
+
+console.log(suma(10,5));
+
+if (c<5)
+{
+    console.log ("el numwro es menor a 5")
+}
+else {
+    console.log ("el numero es menor a 5")
+}
+
+
+
+
+var edad = 20;
+
+if(edad ===18) {
+    console.log("podes votar");
+} else if (edad > 18) {
+    console.log("podes votar de nuevo")
+} else {
+    console.log("aun no podes votar")
+}
+
+
+function saludarEstudiante(estudiante){
+    console.log("hola" + estudiante);
+}
+saludarEstudiante("Rodrigo")
+
+
+var a;
+var b;
+var c;
+function multiplicacion (a,b)
+{
+    return b= a*c;
+}
+console.log(multiplicacion(20,6))
+if (b<100)
+{
+    console.log ("el numero es mayor a 100")
+}
+else
+{
+    console.log ("el numero es menor a 100")
+}
+
+
+let a; 
+let b; 
+let c; 
+function resta (a,b) 
+{
+    return c=a - b; 
+}
+    console.log (resta(59,36));
+    switch(Number) 
+{
+    case 1: "El número es 30" 
+    break;
+    case 2: "El número es 25" 
+    break;
+    case 3: "El número es 23"
+    break;
+    default: "El número es menor a 22" 
+}
